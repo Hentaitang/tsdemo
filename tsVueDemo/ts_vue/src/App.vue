e4fceaf54b4dfb765393cad44a46fdc2aeb381a4
Hentaitang <mz374427166@gmail.com>
2019-07-15 12:59:44 +0800

    todoList

---
+++ tsVueDemo/ts_vue/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <Hentaitang></Hentaitang>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import Hentaitang from "./components/Hentaitang.vue";
+
+@Component({
+  components: {
+    // HelloWorld
+    Hentaitang
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tsVueDemo/ts_vue/src/components/Hentaitang.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    hello world
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./HelloWorld.vue";
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Hentaitang extends Vue {}
+</script>
